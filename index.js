0831ba84b650ab9499afa8c72b12bd34ffed250b
Abdikalik Dini <adini18@gmail.com>
2023-10-10 21:25:39 -0400

    started index.js and created shape and qestions.js files

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions.js');
+const shapes = require('./lib/shape.js');
+
+function generateLogo(svg, data){
+    fs.generateLogo
+}
