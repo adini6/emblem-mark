bc3b0d23c1617b0868ea480a76957839d4ae7db0
Abdikalik Dini <adini18@gmail.com>
2023-10-10 21:59:16 -0400

    added inquirer prompt and imports

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const fs = require('fs');
 const questions = require('./lib/questions.js');
 const { Triangle, Circle, Square } = require('./lib/shape.js');
 
+inquirer.prompt(questions).then((answers) => {
+  let shape;
+  switch (answers.shape) {
+    case 'triangle':
+      shape = new Triangle(answers.shapeColor);
+      break;
+    case 'square':
+      shape = new Square(answers.shapeColor);
+      break;
+    case 'circle':
+      shape = new Circle(answers.shapeColor);
+      break;
+  }
 
-function generateLogo(svg, data){
-    fs.generateLogo
-}
+
+  fs.writeFileSync('logo.svg', svgContent, 'utf-8');
+  console.log('Generated logo.svg');
+});
