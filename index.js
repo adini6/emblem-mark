5dd073975bbcc11d03a52d50723976bedab506de
Abdikalik Dini <adini18@gmail.com>
2023-10-10 22:48:42 -0400

    debugg

--- index.js
+++ index.js
@@ -7,22 +7,28 @@ inquirer.prompt(questions).then((answers) => {
   let shape;
   switch (answers.shape) {
     case 'triangle':
-      shape = new Triangle(answers.shapeColor);
+      shape = new Triangle(answers.logoColor);
       break;
     case 'square':
-      shape = new Square(answers.shapeColor);
+      shape = new Square(answers.logoColor);
       break;
     case 'circle':
-      shape = new Circle(answers.shapeColor);
+      shape = new Circle(answers.logoColor);
       break;
   }
 
+  const shapeSVG = shape.render();
+  
+  const textSVG = `<text x="150" y="120" font-family="Arial" font-size="25" fill="${answers.textColor}" text-anchor="middle">${answers.text}</text>`;
+  
   const logoData = `
     <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      ${shape.render()}
-      <!-- You can also render the text here if needed -->
+      ${shapeSVG}
+      ${textSVG}
     </svg>
-  `;
+`
+;
+
 
 
   fs.writeFileSync('logo.svg', logoData, 'utf-8');
