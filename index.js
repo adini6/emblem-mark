88f174a947f364e6addd48a72790a07d1446ba6c
Abdikalik Dini <adini18@gmail.com>
2023-10-10 21:50:08 -0400

    test.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./lib/questions.js');
-const shapes = require('./lib/shape.js');
+const { Triangle, Circle, Square } = require('./lib/shape.js');
+
 
 function generateLogo(svg, data){
     fs.generateLogo
